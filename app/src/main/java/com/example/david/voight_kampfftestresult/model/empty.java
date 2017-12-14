ccc6d357f512bf8d3bd7fbf57f96371e372bc0d7
David Thai <david.thai22@gmail.com>
2017-12-14 11:21:36 -0600

    removed unused files

--- app/src/main/java/com/example/david/voight_kampfftestresult/model/empty.java
+++
@@ -1,8 +0,0 @@
-package com.example.david.voight_kampfftestresult.model;
-
-/**
- * Created by David on 2017-12-11.
- */
-
-public class empty {
-}
--- app/src/main/java/com/example/david/voight_kampfftestresult/view/empty.java
+++
@@ -1,8 +0,0 @@
-package com.example.david.voight_kampfftestresult.view;
-
-/**
- * Created by David on 2017-12-11.
- */
-
-public class empty {
-}
